3265eeb6c5e7ad5a0139b19aa3a22f89122025cb
hankd11234 <hankd11234@gmail.com>
2021-02-18 13:53:30 -0500

    Add picture's axios calls

---
+++ src/api/pictures.js
@@ -0,0 +1,40 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+export const pictureIndexAll = () => {
+  return axios({
+    url: apiUrl + '/pictures/all/',
+    method: 'GET'
+  })
+}
+
+export const pictureIndex = user => {
+  return axios({
+    url: apiUrl +'/pictures/',
+    method: 'GET',
+    headers: {
+      'Authorization': `Token ${user.token}`
+    }
+  })
+}
+
+export const pictureCreate = (picture, user) => {
+  return axios({
+    url: apiUrl + '/pictures/',
+    method: 'POST',
+    headers: {
+      'Authorization': `Token ${user.token}`
+    },
+    data: { picture: picture }
+  })
+}
+
+export const pictureShow = (id, user) => {
+  return axios({
+    url: apiUrl + '/pictures/' + id,
+    method: 'GET',
+    headers: {
+      'Authorization': `Token ${user.token}`
+    }
+  })
+}
